9ee9f415317bd594f86005eb638972af7c40fbd5
Daniel Escobar <escobard90@gmail.com>
2018-03-14 14:57:19 -0600

    feat: added base state handlers for the manager public key

--- lottery-app/client/src/App.js
+++ lottery-app/client/src/App.js
@@ -3,18 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 import web3 from "./web3";
+import lottery from "./lottery";
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state={
+      manager: 'Loading...'
+    }
+  }
+
+  async componentDidMount(){
+
+    // we don't have to define the "from" property will call if we already have a contract created
+    // since the call will automatically assume you are calling a function from the first account
+    // on metamask
+    const manager = await lottery.methods.manager().call();
+    this.setState({manager})
+
+  }
   render() {
+    let { manager } = this.state
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Lottery Contract</h2>
+        <p>This contract is managed by {manager}</p>
       </div>
     );
   }
--- lottery-app/client/src/lottery.js
+++ lottery-app/client/src/lottery.js
@@ -6,8 +6,8 @@ import web3 from "./web3";
 // for 0.45+, try to add a script that allows the user to create a contract,
 // as long as the user provides an address
 
-
 // the address, and the abi were grabbed from the /ethereum/deploy.js console logs, after deploying to rinkby
+// for this project, we really want to focus only on a single instance of the contract, 
 let address = "0x1785bdC6eE6160a93166643dC396C64876bDACC1",
 abi = '[{"constant":true,"inputs":[],"name":"getBalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]'
 
